1747f4b343baae84c77201ab782353f3dd8626a9
helloworld <kawaii_xw@163.com>
2024-12-22 15:49:19 +0800

    change logo to svg for wrangler load

--- sparkling/portal/src/components/logo.js
+++ sparkling/portal/src/components/logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logoImage from '../assets/icons/logo.png';
+import logoImage from '../assets/icons/logo.svg';
 
 export const Logo = ({ className }) => (
   <img src={logoImage} alt="Smart Memo Logo" className={className} />
